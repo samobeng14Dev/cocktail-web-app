121c3d0be83764abcad4a0255ccf6af58254d544
samuelobeng102@gmail.com <obengsamuel270@gmail.com>
2023-11-23 12:44:24 +0000

    added files for cocktail app

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+	About,
+	HomeLayout,
+	Landing,
+	Error,
+	NewsLetter,
+	Cocktail,
+	SinglePageError,
+} from "./pages";
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/Cocktail";
+import { action as newsLetterAction } from "./pages/NewsLetter";
+
+//loaders are not hooks
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 5,
+		},
+	},
+});
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+				errorElement: <SinglePageError />,
+				loader: landingLoader(queryClient),
+			},
+			{
+				path: "cocktail/:id",
+				element: <Cocktail />,
+				errorElement: <SinglePageError />,
+				loader: singleCocktailLoader(queryClient),
+			},
+			{
+				path: "newsletter",
+				element: <NewsLetter />,
+				errorElement: <SinglePageError />,
+				action: newsLetterAction,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+		],
+	},
+]);
+
+const App = () => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+};
+export default App;
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,27 @@
+import Wrapper from "../assets/wrappers/SearchForm";
+import { Form, useNavigation } from "react-router-dom";
+
+const SearchForm = ({ SearchForm }) => {
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
+	return (
+		<Wrapper>
+			<Form className='form'>
+				<input
+					type='search'
+					name='search'
+					className='form-input'
+					defaultValue={SearchForm}
+				/>
+				<button
+					type='submit'
+					className='btn'
+					disabled={isSubmitting}>
+					{isSubmitting ? "searching..." : "search"}
+				</button>
+			</Form>
+		</Wrapper>
+	);
+};
+
+export default SearchForm;
